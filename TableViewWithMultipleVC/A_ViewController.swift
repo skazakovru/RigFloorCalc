332b2efc4b7c343f61a0b6b41339ab2f2a8c68bc
Sergei Kazakov <skazakovru@yahoo.com>
2017-02-20 18:46:50 +0300

    KWM calculator works and formatting done

--- TableViewWithMultipleVC/A_ViewController.swift
+++ TableViewWithMultipleVC/A_ViewController.swift
@@ -10,7 +10,14 @@ import UIKit
 
 class A_ViewController: UIViewController {
 
+    @IBOutlet weak var originalMWTextField: UITextField!
+    @IBOutlet weak var shutIDPPtextField: UITextField!
+    @IBOutlet weak var trueVDtextField: UITextField!
+    @IBOutlet weak var metersTVDtextField: UITextField!
+    @IBOutlet weak var outputTextField: UITextField!
+    
     override func viewDidLoad() {
+   
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
@@ -21,6 +28,26 @@ class A_ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
-    
+    @IBAction func calculateTappedButton(_ sender: Any) {
+        let firstValue = Double(originalMWTextField.text!)
+        let secondValue = Double(shutIDPPtextField.text!)
+        let thirdValue = Double(trueVDtextField.text!)
+        let fourthValue = Double(metersTVDtextField.text!)
+        
+        if firstValue != nil && secondValue != nil && thirdValue != nil {
+            
+            let outputValue = Double((secondValue! * 19.23)/thirdValue! + firstValue!)
+            
+            outputTextField.text = String(format:"%.2f", outputValue)
+            
+        } else if firstValue != nil && secondValue != nil && thirdValue != nil || fourthValue != nil {
+            
+            let outputValue = Double((secondValue! * 19.23)/(fourthValue! * 3.281) + firstValue!)
+             outputTextField.text = String(format:"%.2f", outputValue)
+        }
+}
+    @IBAction func gestureTapped(_ sender: Any) {
+        view.endEditing(true)
+    }
 
 }
