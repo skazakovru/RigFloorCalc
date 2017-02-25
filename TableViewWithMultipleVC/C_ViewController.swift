391cd8c51434bb2ce0560acd908cc33b99e21cf5
Sergei Kazakov <skazakovru@yahoo.com>
2017-02-25 22:41:27 +0300

    3rd calculator interface is build

---
+++ TableViewWithMultipleVC/C_ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  C_ViewController.swift
+//  TableViewWithMultipleVC
+//
+//  Created by Sergei Kazakov on 24/02/2017.
+//  Copyright © 2017 Sergei Kazakov. All rights reserved.
+//
+
+import UIKit
+
+class C_ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+
+}
